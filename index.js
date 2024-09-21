464c3d7cb7401b65a54acb4c661fcff5314dc448
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2024-09-21 14:54:51 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const axios = require('axios');
-const city = "Sao Caetano do Sul";
-const key = process.env.API_KEY
-
 require('dotenv').config();
 
-async function fetchData() {
+const axios = require('axios');
+const key = process.env.API_KEY
+
+
+async function getCoordsByName(cityName) {
     try {
-        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`);
-        console.log(response.data);
+        const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`);
+        let latitude = response.data["0"]["lat"]
+        let longitude = response.data["0"]["lon"]
+
+        return {
+            latitude,
+            longitude
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-fetchData();
+
+async function weatherByName(cityName) {
+    try {
+
+        const coords = await getCoordsByName(cityName)
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${key}`)
+
+        const feelsLike = response.data["main"]["feels_like"]
+        const description = response.data["weather"]["0"]["description"]
+
+        return {
+            feelsLike,
+            description
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+
+}
+
+weatherByName("Manaus").then(data => console.log(data))
